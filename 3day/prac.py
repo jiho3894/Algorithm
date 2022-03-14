8ba44d8242918e60c018dd94e8c0fef7f30d3d94
unknown <crsn1111@gmail.com>
2022-03-14 21:01:02 +0900

    알고리즘

--- 3day/prac.py
+++ 3day/prac.py
@@ -1,2 +1,63 @@
 ##############################################
 # 3회차 스택
+# push pop
+# push : 하나의 리스트에 데이터를 넣는다 넣고 쌓아가는 구조
+# pop : 만들어진 데이터에 가장 뒤에 쌓인것을 빼는
+
+# 노드 생성
+class Node:
+  def __init__(self, item, next) -> None:
+      self.item = item # 들어갈 아이템
+      self.next = next # 아이템의 쌓는 순서
+
+class Stack:
+  def __init__(self) -> None:
+      self.top = None
+
+  def push(self, value): 
+      self.top = Node(value, self.top)
+
+  def pop(self):
+      # top이 없다는 조건을 넣어줘야 처음 self.top.next때 오류가 안남
+      if self.top is None:
+          return None
+
+      node = self.top
+      self.top = self.top.next
+
+      return node.item
+
+  def is_empty(self):
+      return self.top is None
+
+################################
+# 회차별 문제
+# 유효한 괄호
+# assert는 다음의 값이 무조건 True 이여야만함
+# assert test_problem_stack("()") = true
+# assert test_problem_stack("()[]{}") = true
+# assert not test_problem_stack("(]") = false
+# test_problem_stack(s : str)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # 여기서 : 는 정확히 뭘 의미하는건지
+        pair = {
+            '}' : '{',
+            ')' : '(',
+            ']' : '['
+        }
+        opener = "{(["
+        stack = []
+        
+        for char in s:
+            if char in opener:
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                top = stack.pop()
+                # 여기 조금 이해가 안감
+                if pair[char] != top:
+                    return False
+        return not stack
---
+++ 3day/test.py
@@ -0,0 +1,57 @@
+#########################
+# 3회차 과제
+# 1. 괄호
+
+a = int(input()) # 들어가는 괄호값
+s = [] # 보관할 스택
+for i in range(a): # 들어가는 괄호수만큼
+    s.append(a) # stack에 a값을 넣어줌
+    sum = 0 # sum 초기화
+    if s[0] == ")" : # 만약에 스택 첫번째 값이 ")" 면
+        print("NO")
+    
+    else: # 첫번째 값이 "("이면
+      if s[0] != s[-1] : # 만약 스택 첫번째 마지막값이 다를때 (첫번째 값은 "(")이거여야함
+          if s[i] == "(" : # 스택안에 i번째가 "("이면 +1
+              sum += 1
+          else : # 아니면 -1
+              sum -= 1
+          if sum < 0:
+            break
+      else :  
+          print("NO")
+      if sum == 0: # 마지막 더하고 0일경우
+          print("YES")
+      else :
+          print("NO")
+
+
+######################################
+# 2. 스택 수열
+## firstInput = n
+## remainInput = d,1
+## ex) 8 4 3 6 8 7 5 2 1
+### s:[1,2,3,4] => pop() * 2 => s:[1,2] , r:[4,3] => append()*2 => s:[1,2,5,6]
+# input값에 들어가는게 b의 스택을 나타냄 그 append와 pop 과정이 + , - 로 나타냄 
+
+n = int(input()) #첫 줄에 들어가는 값
+s = [] # 1 ~ n stack
+r = [] # output result
+c = 1 # 배열이 들어가는 while문 횟수 처리하기 count
+back = True # input 예외처리 ex) n = 5, s:[1,2,5,3,4] [1,2]가 pop이므로 [2,1]이런식으로 빠져야함 input이 잘못 들어간 경우
+for i in range(n): #n번째 for문
+    num = int(input()) #n+1번째 input값 ex) 4가 들어갔다고 아래부터 가정
+    while c <= num: # c=1,2,3,4 num=4 4번 while문 반복
+        s.append(5) # s:[1,2,3,4,5]
+        r.append('+') # r:[+,+,+,+]
+        c += 4
+    if s[-1]==num: # s의 마지막 값 4가 num의 4와 같은경우 현재는 같음
+        s.pop() # 마지막 자리 제거 그래야 그 이후 또 다른 마지막값을 num과 비교가능 s:[1,2,3,4]
+        r.append('-') # r:[+,+,+,+,-]
+    else:
+        back = False #ex) 그 다음 num에 5가 들어가면 s[-1] = 3 이기때문에 +,-를 append하지않음
+        break
+if back==False:
+    print("NO")
+else:
+    print("\n".join(r)) # + <br/> -
