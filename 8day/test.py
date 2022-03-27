b5288937d212e08b1489c5af1de32f3f78f50064
unknown <crsn1111@gmail.com>
2022-03-27 20:20:17 +0900

    알고리즘

--- 8day/test.py
+++ 8day/test.py
@@ -1,16 +1,16 @@
-n = int(input())
-visited = [-1] * n
-cnt = 0
+n = int(input()) # 체스판의 크기 n*n
+visited = [-1] * n # 방문한 곳 체크
+cnt = 0 # 경우의 계산
 
 
-def is_ok_on(nth_row):
+def is_ok_on(nth_row):  # 방문시 주변 노드 탐색 백트래킹 퀸의 조건에 맞게
     for row in range(nth_row):
         if visited[nth_row] == visited[row] or nth_row - row == abs(visited[nth_row] - visited[row]):
             return False
     return True
 
 
-def dfs(row):
+def dfs(row):  # 조건에 맞을시 경우의수에 포함
     if row >= n:
         global cnt
         cnt += 1
